0edc780ef937dad71cd76efc4ba8ef13e17c5b51
JasonKingAMS <info@jasonaking.nl>
2017-09-26 23:10:20 +0200

    Add association between user and files

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926205926) do
+ActiveRecord::Schema.define(version: 20170926210449) do
 
   create_table "docs", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
